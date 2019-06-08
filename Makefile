6c65f51ddb8e4b73f6059f3b86a9fd5f98198cb8
Zachary Cao <caozengle@163.com>
2019-06-08 16:15:04 +0800

    1. 修改了底层中文支持为 ctex； 2. 新增了 Windows 下 RUNME.bat 新手脚本。

--- Makefile
+++ Makefile
@@ -19,7 +19,6 @@ SAMPLECONTENTS=$(SAMPLE).tex
 SAMPLEBIB=$(SAMPLE).bib
 INSTITUTE_LOGO=njulogo.eps
 INSTITUTE_NAME=njuname.eps
-TEXMFLOCAL=$(shell get_texmf_dir.sh)
 
 .PHONY: all clean cls doc sample
 
---
+++ RUNME.bat
@@ -0,0 +1,21 @@
+﻿:: UTF-8
+chcp 65001
+@ echo off
+:: 运行安装程序
+xelatex njuthesis.ins
+:: 生成手册
+xelatex njuthesis.dtx
+makeindex -s gind.ist -o njuthesis.ind njuthesis.idx
+xelatex njuthesis.dtx
+xelatex njuthesis.dtx
+:: 生成示例文档
+xelatex sample.tex
+bibtex sample
+xelatex sample.tex
+xelatex sample.tex
+:: 删除辅助文件
+del *.aux *.bak *.bbl *.blg *.dvi *.glo *.gls *.idx *.ilg *.ind *.ist ^
+    *.log *.out *.ps *.thm *.toc *.lof *.lot *.loe
+cls
+echo 恭喜！您已生成其他的基本文件和说明文档，这说明您的写作环境没有问题!
+pause
